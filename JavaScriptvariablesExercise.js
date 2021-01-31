17c6fd90e2c8f95814c2523d18f16706812a8ec7
Emmanuel Isidahomen <50160339+Emmanuelisi20@users.noreply.github.com>
2021-01-30 19:05:08 -0500

    Create JavaScriptvariablesExercise.js
    added a new java exercise

---
+++ JavaScriptvariablesExercise.js
@@ -0,0 +1,19 @@
+/********** Use ing this to move numbers around *******/
+
+function test() {
+    var a = "3";
+    var b = "8";
+    
+/***********Do not change the code above 👆*******/
+//Write your code on lines 7 - 9:
+ 
+ var c = a;
+ a = b;
+ b = c;
+
+    
+/***********Do not change the code below 👇*******/
+
+    console.log("a is " + a);
+    console.log("b is " + b);
+}
