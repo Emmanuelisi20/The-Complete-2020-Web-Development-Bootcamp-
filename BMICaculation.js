1a051250882534fb239f96c54eed63bf62799831
Emmanuel Isidahomen <50160339+Emmanuelisi20@users.noreply.github.com>
2021-01-31 20:03:17 -0500

    Create BMICaculation.js

---
+++ BMICaculation.js
@@ -0,0 +1,23 @@
+//creating a funtion 
+
+function bmiCalculator(weight, height) {
+
+var bmi = weight / (height * height);
+
+return bmi;
+
+}
+
+
+var bmi = bmiCalculator(65, 1.8);
+
+//function aFunction () {
+//    var a = 12;
+//    var b = 7;
+//    return a - b;
+//}
+
+//function aFunction(a, b){
+//return a * b;
+//}
+//aFunction(3,4);
