71a7818ff1fbdfad119d6ea76a41bba4df8636ef
Emmanuel Isidahomen <50160339+Emmanuelisi20@users.noreply.github.com>
2021-01-31 23:59:26 -0500

    Create lovecalculatorrandom.js

---
+++ lovecalculatorrandom.js
@@ -0,0 +1,17 @@
+prompt("What is your name ");
+prompt("what is your name ");
+
+var loveScore = Math.random() * 100;
+loveScore = Math.floor(loveScore)+1;
+
+//trying a create a logical if -statament 
+
+if (loveScore > 100 )
+
+{
+alert(" your love score is " + loveScore + "%" + " you love each other so much ");
+}
+else 
+{
+alert("your love score is " + loveScore + "%");
+}
